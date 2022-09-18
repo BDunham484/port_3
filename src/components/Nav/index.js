3eb6a6817ab87ca4ec23b70982a54b3095795d58
ThereWolf <bdunham484@gmail.com>
2022-09-18 15:37:47 -0500

    finalize hamburger

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 // import Navigation from './Navigation';
 // import MobileNavigation from './MobileNavigation';
+import { useState } from 'react';
 import { pageReload } from '../../utils/helpers';
+import { VscMenu, VscChromeClose } from 'react-icons/vsc';
+import { motion } from 'framer-motion';
 
 const Nav = (props) => {
     const {
@@ -14,12 +17,18 @@ const Nav = (props) => {
         contactSelected,
         setContactSelected
     } = props;
+    const [open, setOpen] = useState(false);
+    const hamburgerIcon = <VscMenu id="hamburger" onClick={() => setOpen(!open)}/>
+    const closeIcon = <VscChromeClose id="hamburger" onClick={() => setOpen(!open)}/>
+    const closeMobileMenu = () => setOpen(false);
+    const animateFrom = { opacity: 0, y: -80 }
+    const animateTo = { opacity: 1, y: 0 }
     return (
         //HEADER/NAV
         <header>
         <div>
             <h1>
-                <a href="#home" onClick={() => {setAboutSelected(false);setProjectsSelected(false);setResumeSelected(false);setContactSelected(false);pageReload()}}>
+                <a href="#home" onClick={() => {setAboutSelected(false);setProjectsSelected(false);setResumeSelected(false);setContactSelected(false);closeMobileMenu();pageReload()}}>
                 Brad Dunham
                 </a>
             </h1>
@@ -45,20 +54,34 @@ const Nav = (props) => {
             </ul>
         </nav >
         < nav id="MobileNavigation">
-            <ul>
-                <li className={`${aboutSelected && 'navActive'}`}>
-                    <a href="#about" onClick={() => { setAboutSelected(true); setProjectsSelected(false); setResumeSelected(false); setContactSelected(false) }}>About</a>
-                </li>
-                <li className={`${projectsSelected && 'navActive'}`}>
-                    <a href="#projects" onClick={() => { setAboutSelected(false); setProjectsSelected(true); setResumeSelected(false); setContactSelected(false) }}>Projects</a>
-                </li>
-                <li className={`${resumeSelected && 'navActive'}`}>
-                    <a href="#resume" onClick={() => { setAboutSelected(false); setProjectsSelected(false); setResumeSelected(true); setContactSelected(false) }}>Resume</a>
-                </li>
-                <li className={`${contactSelected && 'navActive'}`}>
-                    <a href="#contact" onClick={() => { setAboutSelected(false); setProjectsSelected(false); setResumeSelected(false); setContactSelected(true) }}>Contact</a>
-                </li>
-            </ul>
+            {/* <VscMenu id="hamburger" onClick={() => setOpen(!open)}/> */}
+            {open ? closeIcon : hamburgerIcon}
+            {open && <ul>
+                <motion.li initial={animateFrom}
+                animate={animateTo}
+                transition={{delay: 0.05}}
+                className={`${aboutSelected}`}>
+                    <a className="navMobileLinks" href="#about" onClick={() => { setAboutSelected(true); setProjectsSelected(false); setResumeSelected(false); setContactSelected(false);closeMobileMenu() }}>About</a>
+                </motion.li>
+                <motion.li initial={animateFrom}
+                animate={animateTo}
+                transition={{delay: 0.10}}
+                className={`${aboutSelected}`}>
+                    <a className="navMobileLinks" href="#projects" onClick={() => { setAboutSelected(false); setProjectsSelected(true); setResumeSelected(false); setContactSelected(false);closeMobileMenu() }}>Projects</a>
+                </motion.li>
+                <motion.li initial={animateFrom}
+                animate={animateTo}
+                transition={{delay: 0.20}}
+                className={`${aboutSelected}`}>
+                    <a className="navMobileLinks" href="#resume" onClick={() => { setAboutSelected(false); setProjectsSelected(false); setResumeSelected(true); setContactSelected(false);closeMobileMenu() }}>Resume</a>
+                </motion.li>
+                <motion.li initial={animateFrom}
+                animate={animateTo}
+                transition={{delay: 0.30}}
+                className={`${aboutSelected}`}>
+                    <a className="navMobileLinks" href="#contact" onClick={() => { setAboutSelected(false); setProjectsSelected(false); setResumeSelected(false); setContactSelected(true);closeMobileMenu() }}>Contact</a>
+                </motion.li>
+            </ul>}
         </nav >
         {/* <Navigation></Navigation>
         <MobileNavigation></MobileNavigation> */}
