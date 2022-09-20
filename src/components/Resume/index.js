65a86ab2822541aed253e781cd87a5d3250864a8
ThereWolf <bdunham484@gmail.com>
2022-09-19 21:20:19 -0500

    lower landscape media queries

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import ResumePDF from '../../assets/images/Resume.pdf';
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai'
+import { AiOutlineDownload } from 'react-icons/ai';
 import { Document, Page, pdfjs } from 'react-pdf'
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 const Resume = () => {
@@ -11,12 +12,17 @@ const Resume = () => {
     }
     const leftArrrow = <AiOutlineArrowLeft id="left-arrow" onClick={() => setPageNumber(1)} />
     const rightArrow = <AiOutlineArrowRight id="right-arrow" onClick={() => setPageNumber(2)} />
+    const downloadButton = <a href={ResumePDF} target="_blank" rel="noreferrer"><AiOutlineDownload id="download-button"/></a>
     return (
         <section id="resume">
+            <span id="resume-title-wrapper">
             <h2>
                 RESUME
             </h2>
-            <Document file={ResumePDF} onLoadSuccess={onDocumentLoadSuccess}>
+            {downloadButton}
+            </span>
+            
+            <Document id="resume" file={ResumePDF} onLoadSuccess={onDocumentLoadSuccess}>
                 <Page pageNumber={pageNumber} />
             </Document>
             <div id="page-selection">
