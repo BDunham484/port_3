a77f52be0961b3d957741af38907fe1cc343a220
ThereWolf <bdunham484@gmail.com>
2022-09-23 13:05:52 -0500

    add description styles

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -12,28 +12,29 @@ const Portfolio = (props) => {
             <div className="project-wrapper">
                 {projects.map((project) => (
                     <div>
-                    <div className="project-title-wrapper">
-                        <p className='wrapped-titles'>
-                            {project.name}
-                        </p>
-                        <span>
-                            <a className="githubLogo" href={project.githubLink} target="_blank" rel="noreferrer">
-                                <img src={githubLogo} alt="Github Logo" />
-                            </a>
-                        </span>
-                    </div>
-                    <a href={project.projectLink} target="_blank" rel="noreferrer"><img className="wrapped-img"
-                    src={project.img}
-                    alt={project.alt} />
-                    </a>
-            
-                    <div className="project-description">
-                        <div className="project-text-container">
-                            <div className="project-text">
-                                {project.projectText}
+                        <div className="project-title-wrapper">
+                            <p className='wrapped-titles'>
+                                {project.name}
+                            </p>
+                            <span>
+                                <a className="githubLogo" href={project.githubLink} target="_blank" rel="noreferrer">
+                                    <img src={githubLogo} alt="Github Logo" />
+                                </a>
+                            </span>
+                        </div>
+                        <a href={project.projectLink} target="_blank" rel="noreferrer"><img className="wrapped-img"
+                        src={project.img}
+                        alt={project.alt} />
+                        </a>
+                        <div className='description-wrapper'>
+                            <div className="project-description">
+                                <div className="project-text-container">
+                                    <div className="project-text">
+                                        {project.projectText}
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
                     </div>
                 ))}
             </div>
